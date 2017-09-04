c76870fa2331213a071b0aa84a6fa638185e4e84
Balint Molnar <molbal@outlook.com>
2017-09-04 23:50:14 +0200

    Added notifications
    The taskbar icon will flash and change color if a new message arrives.

--- src/background.js
+++ src/background.js
@@ -9,30 +9,31 @@ import { app, Menu } from 'electron';
 import { devMenuTemplate } from './menu/dev_menu_template';
 import { editMenuTemplate } from './menu/edit_menu_template';
 import createWindow from './helpers/window';
-
 import { shell } from 'electron';
+
+// Notification state
+global.notificationState = {
+  flashing: false
+};
+const nativeImage = require('electron').nativeImage;
 
 const iconUrl = url.format({
   pathname: path.join(__dirname, 'Icon/Icon.icns'),
   protocol: 'file:',
   slashes: true
-})
+});
 
-// Set user agent
-//import { session } from 'electron';
+const peaceIcon = nativeImage.createFromPath(path.join(__dirname, 'icons/peace.ico'));
+const actionIcon = nativeImage.createFromPath(path.join(__dirname, 'icons/action.ico'));
+
+
+
+
 
 // Special module holding environment variables which you declared
 // in config/env_xxx.json file.
 import env from './env';
-//
-//const setApplicationMenu = () => {
-//  const menus = [editMenuTemplate];
-//  if (env.name !== 'production') {
-//    menus.push(devMenuTemplate);
-//  }
-//  Menu.setApplicationMenu(Menu.buildFromTemplate(menus));
-//};
-//
+
 // Save userData in separate folders for each environment.
 // Thanks to this you can use production and development versions of the app
 // on same machine like those are two separate apps.
@@ -46,14 +47,30 @@ app.on('ready', () => {
  // setApplicationMenu();
 
   const mainWindow = createWindow('main', {
-    width: 1000,
-    height: 600
+    width: 1280,
+    height: 720
   });
 
   mainWindow.loadURL("https://messenger.com/login.php");
   mainWindow.webContents.on('new-window', (event, url) => {
     event.preventDefault();
     shell.openExternal(url);
+  });
+
+  // On a new message a title like this: (1) Messenger
+  mainWindow.on('page-title-updated', (event, title) => {
+    if (!global.notificationState.flashing && title.startsWith("(")) {
+      mainWindow.flashFrame(true);
+      global.notificationState.flashing = true;
+      mainWindow.setIcon(actionIcon);
+       
+    }
+  });
+
+  mainWindow.on('focus', (event, title) => {
+      mainWindow.flashFrame(false);
+      global.notificationState.flashing = false;
+      mainWindow.setIcon(peaceIcon);
   });
 });
 
